708f42d4a784cd86b8cf6de65d6d678dffcc12e5
Ukiyo0603 <shivanimakde01@gmail.com>
2023-05-08 02:17:34 +0530

    Models changes

--- client_app/models.py
+++ client_app/models.py
@@ -81,25 +81,7 @@ class Invman(models.Model):
     
 # class Brand(models.Model):
 #     bname=models.CharField(max_length=50, default="Nike")
-#     vendors= models.ForeignKey(Vendor,on_delete=models.CASCADE,default=1 )
-class Order(models.Model):
-    product = models.ForeignKey(Product,
-                                on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer,
-                                 on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-    price = models.IntegerField()
-    address = models.CharField (max_length=50, default='', blank=True)
-    phone = models.CharField (max_length=50, default='', blank=True)
-    date = models.DateField (default=datetime.datetime.today)
-    status = models.BooleanField (default=False)
-
-    def placeOrder(self):
-        self.save()
-
-    @staticmethod
-    def get_orders_by_customer(customer_id):
-        return Order.objects.filter(customer=customer_id).order_by('-date')
+#     vendors= models.ForeignKey(Vendor,on_delete=models.CASCADE,default=1 
 
 
 class Product(models.Model):
@@ -126,6 +108,25 @@ class Product(models.Model):
             return Product.objects.filter (category=category_id)
         else:
             return Product.get_all_products()
+        
+class Order(models.Model):
+    product = models.ForeignKey(Product,
+                                on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    address = models.CharField (max_length=50, default='', blank=True)
+    phone = models.CharField (max_length=50, default='', blank=True)
+    date = models.DateField (default=datetime.datetime.today)
+    status = models.BooleanField (default=False)
+
+    def placeOrder(self):
+        self.save()
+
+    @staticmethod
+    def get_orders_by_customer(customer_id):
+        return Order.objects.filter(customer=customer_id).order_by('-date')
 # Create your models here.
 
 
