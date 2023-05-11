7f26a6b1cc87d1df28a5cb90b10b8e5d905fd76c
Harsh Pathekar <96983175+pathekarharsh@users.noreply.github.com>
2023-05-11 16:55:23 +0530

    added subcategory page

--- customer-frontend/src/App.js
+++ customer-frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./routes/LoginPage";
 import Home from "./routes/Home";
-import Product from "./routes/Product";
+import SubCategory from "./routes/SubCategory";
 import ProductDetail from "./routes/ProductDetail";
 import PlaceOrder from "./routes/PlaceOrder";
 import SignUp from "./routes/SignUp";
@@ -26,7 +26,7 @@ function App() {
         <Route exact path="/" element={<SignUp />} />
         <Route exact path="/login" element={<LoginPage />} />
         <Route exact path="/home" element={<Home />} />
-        <Route exact path="/product" element={<Product />} />
+        <Route exact path="/subcategory" element={<SubCategory />} />
         <Route exact path="/productdetails" element={<ProductDetail />} />
         <Route exact path="/placeorder" element={<PlaceOrder />} />
 
---
+++ customer-frontend/src/routes/SubCategory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from "../Components/SubCategory/Main"
+
+const  SubCategory = () => {
+  return (
+      <div>
+          <Main />
+    </div>
+  )
+}
+
+export default SubCategory
